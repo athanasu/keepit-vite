086c886453a506e153160b4a83a9e276d25be4b0
Georgios Athanasoulias <georgios@paddle.com>
2023-03-06 19:22:27 +0000

    setup vitest / react-testing-library

---
+++ src/components/app/app.test.tsx
@@ -0,0 +1,7 @@
+import { expect, it } from 'vitest'
+
+it('Math.sqrt()', () => {
+  expect(Math.sqrt(4)).toBe(2)
+  expect(Math.sqrt(144)).toBe(12)
+  expect(Math.sqrt(2)).toBe(Math.SQRT2)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
@@ -12,4 +14,9 @@ export default defineConfig({
     },
   },
   plugins: [react(), svgr(), eslintPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
 })
